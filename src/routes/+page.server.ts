b1f71143ff7b7230e6c58035f6dcd171c544c3d4
Mao <m@maowtm.org>
2024-06-22 15:34:04 +0100

    Upgrade SvelteKit

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,11 +5,10 @@ import { getSiteStats } from '$lib/server/misc';
 export const load: PageServerLoad = async ({ cookies, fetch }) => {
 	let homeDataFetch = fetch('/api/v1/user/01HEDRH973KWC3BF1EM7VREWD2').then(async (r) => {
 		if (!r.ok) {
-			error(r.status, await r.json());
+			throw error(r.status, await r.json());
 		}
 		return await r.json();
 	});
-	homeDataFetch.catch(() => { /* no-op to catch error thrown before promise is pulled by svelte: https://kit.svelte.dev/docs/load#streaming-with-promises */ });
 	let siteStats = await getSiteStats().then(null, e => Promise.reject(error(500, { message: "Failed to fetch site statistics: " + e })));
 	return {
 		homeData: homeDataFetch,
