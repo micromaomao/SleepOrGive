28543fdee95f696173d199ba8e5d959f4ec1f589
Mao <m@maowtm.org>
2024-06-22 16:16:54 +0100

    username-based homepage info fetch

--- src/lib/server/misc.ts
+++ src/lib/server/misc.ts
@@ -4,6 +4,7 @@ import { withDBClient, type Client as DBClient } from './db';
 export async function getSiteStats(): Promise<SiteStats> {
   return await withDBClient(async (db: DBClient) => {
     let { rows } = await db.query('select (select count(*)::integer from users) as "nbUsers", 0 as "totalAmountDonated"');
+    // TODO: need to deal with potentially different currency
     return rows[0] as SiteStats;
   });
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,16 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { getSiteStats } from '$lib/server/misc';
+import { usernameToId } from '$lib/server/user';
 
 export const load: PageServerLoad = async ({ cookies, fetch }) => {
-	let homeDataFetch = fetch('/api/v1/user/01HEDRH973KWC3BF1EM7VREWD2').then(async (r) => {
+	let homeDataFetch = usernameToId('maowtm').then(maowtm_userid => fetch(`/api/v1/user/${maowtm_userid}`)).then(async (r) => {
 		if (!r.ok) {
 			throw error(r.status, await r.json());
 		}
 		return await r.json();
 	});
+	homeDataFetch.catch(() => { /* no-op to catch error thrown before promise is pulled by svelte: https://kit.svelte.dev/docs/load#streaming-with-promises */ });
 	let siteStats = await getSiteStats().then(null, e => Promise.reject(error(500, { message: "Failed to fetch site statistics: " + e })));
 	return {
 		homeData: homeDataFetch,
